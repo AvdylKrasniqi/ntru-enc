d09d371395c48f7d157a05db512315f5a4c64d1b
Avdyl Krasniqi <iamavdylk@gmail.com>
2023-11-16 09:23:10 +0100

    add interactivity

--- src/main/java/org/fiek/Main.java
+++ src/main/java/org/fiek/Main.java
@@ -3,6 +3,7 @@ package org.fiek;
 import net.sf.ntru.encrypt.EncryptionKeyPair;
 import net.sf.ntru.encrypt.EncryptionParameters;
 import net.sf.ntru.encrypt.NtruEncrypt;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,13 +11,23 @@ import java.util.Scanner;
 
 public class Main {
 
-    public ArrayList<User> users;
+    public static ArrayList<User> users = new ArrayList<User>();
     static String availableOptions = "1) Listo përdoruesit e regjistruar" +
             "\n2) Krijo një përdorues" +
-            "\n3) Shto një përdorues" +
+            "\n3) Shiko public key të një përdorues" +
             "\n4) Enkripto një mesazh" +
             "\n5) Dekripto një mesazh" +
-            "\n6) Dilni";
+            "\n5) Shiko qelesin publik te nje perdoruesit" +
+            "\n100) Dilni";
+
+    public static User findUserByUsername(String username) {
+        for(User user: users) {
+            if(user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
 
@@ -31,6 +42,86 @@ public class Main {
 
             String selectedOption = sc.nextLine();
 
+            if(selectedOption.equals("1")) {
+                for(User user : users) {
+                    System.out.println(user);
+                }
+            }
+            else if(selectedOption.equals("2")) {
+                try {
+                    User u = User.registerForm(sc);
+                    if(u != null) users.add(u);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            else if(selectedOption.equals("3")) {
+                User u = null;
+                while (u == null) {
+
+                    if(users.size() == 0) {
+                        System.out.println("Nuk ekziston ende ndonje perdorues");
+                        break;
+                    }
+
+                    System.out.println("Shkrunai emrin e perdoruesit");
+                    String username = sc.nextLine().trim();
+                    u = findUserByUsername(username);
+                    if(u == null) System.out.println("Ky perdorues nuk ekziston!");
+                    else System.out.println(u.getPublicKeyAsString());
+                }
+            }
+            else if(selectedOption.equals("4")) {
+                User u = null;
+                while (u == null) {
+
+                    if(users.size() == 0) {
+                        System.out.println("Nuk ekziston ende ndonje perdorues");
+                        break;
+                    }
+
+                    System.out.println("Shkrunai emrin e perdoruesit me qelësin publik e të cilit do të enkriptoni mesazhin");
+                    String username = sc.nextLine().trim();
+                    u = findUserByUsername(username);
+                    if(u == null) System.out.println("Ky perdorues nuk ekziston!");
+                    else {
+                        System.out.println("Shkruani mesazhin");
+                        String message = sc.nextLine();
+                        System.out.println("Mesazhi i enkriptuar: ");
+                        System.out.println(u.encryptMessage(message));
+                    };
+                }
+            }
+            else if(selectedOption.equals("5")) {
+                User u = null;
+                while (u == null) {
+                    if(users.size() == 0) {
+                        System.out.println("Nuk ekziston ende ndonje perdorues");
+                        break;
+                    }
+
+                    System.out.println("Shkrunai emrin e perdoruesit me qelësat e të cilit do të dekriptoni mesazhin");
+                    String username = sc.nextLine().trim();
+
+                    u = findUserByUsername(username);
+                    if(u == null) System.out.println("Ky perdorues nuk ekziston!");
+                    else {
+                        System.out.println("Shkruani mesazhin");
+                        String message = sc.nextLine();
+                        try {
+                            System.out.println("Mesazhi i dekriptuar: ");
+                            System.out.println(u.decryptMessage(message));
+                        } catch(Exception e) {
+                            System.out.println("Dicka shkoi gabim!");
+                            System.out.println(e.getMessage());
+                        }
+                    };
+                }
+            }
+            else if(selectedOption.equals("100")) {
+                System.out.println("Faleminderit që zgjodhët FIEKMASTER ~ NTRU");
+                break;
+            }
         }
 
     }
--- src/main/java/org/fiek/User.java
+++ src/main/java/org/fiek/User.java
@@ -1,10 +1,72 @@
 package org.fiek;
 
 import net.sf.ntru.encrypt.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Scanner;
 
 public class User {
     private String username;
     private EncryptionKeyPair keyPair;
+
+    public static User registerForm(Scanner scanner) {
+        System.out.println("Shkruani emrin");
+        String username = scanner.nextLine();
+        System.out.println("Ju vendoset username " + username);
+        EncryptionKeyPair kp = null;
+
+        do {
+            System.out.println("Si të veprohet me qelesat e përdoruesit");
+            System.out.println("1) Krijoni një pair të ri");
+            System.out.println("2) Vendos private key dhe public key manualisht");
+            System.out.println("3) Posedoj vetem public key, vendos vetem ate manualisht");
+            String selectedOption = scanner.nextLine().trim();
+            if(selectedOption.equals("1")) {
+                kp = ntru.generateKeyPair();
+            } else if(selectedOption.equals("2")) {
+                while (true) {
+                    System.out.println("Shkruani private key");
+                    String privKey = scanner.nextLine().trim();
+                    System.out.println("Shkruani public key");
+                    String pubKey = scanner.nextLine().trim();
+                    try {
+                        kp = new EncryptionKeyPair(
+                                new EncryptionPrivateKey(HexUtil.hexStringToByteArray(privKey)),
+                                new EncryptionPublicKey(HexUtil.hexStringToByteArray(pubKey))
+                        );
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Ju lutem vendosni qelesa valid");
+                    }
+                }
+            } else if(selectedOption.equals("3")) {
+                while (true) {
+                    System.out.println("Shkruani public key key");
+                    String privKey = null;
+                    String pubKey = scanner.nextLine().trim();
+                    try {
+                        kp = new EncryptionKeyPair(
+                                null,
+                                new EncryptionPublicKey(HexUtil.hexStringToByteArray(pubKey))
+                        );
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Ju lutem vendosni qelesa valid");
+                        String _continue = scanner.nextLine().trim();
+                        if(_continue.equalsIgnoreCase("no")) {
+                            return null;
+                        }
+                    }
+                }
+            } else {
+                System.err.println("Ju lutem zgjidhni nje nga opcionet e ofruara");
+            }
+        } while (kp == null || username == null || username.equals(""));
+
+        System.out.println("Ju vendoset username " + username);
+
+        return new User(username, kp);
+    }
     static NtruEncrypt ntru = new NtruEncrypt(EncryptionParameters.EES1087EP2_FAST);
 
     public User(String username, EncryptionKeyPair keyPair) {
@@ -21,6 +83,10 @@ public class User {
         return this.keyPair.getPublic();
     }
 
+    public String getPublicKeyAsString() {
+        return HexUtil.byteArrayToHex(getPublicKey().getEncoded());
+    }
+
     public String encryptMessage(String message) {
         byte[] enc = ntru.encrypt(message.getBytes(), this.keyPair.getPublic());
         return HexUtil.byteArrayToHex(enc);
@@ -29,5 +95,18 @@ public class User {
     public String decryptMessage(String hex_message) {
         byte[] encryptedText = HexUtil.hexStringToByteArray(hex_message);
         return new String(ntru.decrypt(encryptedText, keyPair));
+    }
+
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String toString() {
+        return  username +
+                (keyPair.getPrivate() != null ? " ka qeles privat" : "nuk ka qeles privat") +
+                " dhe " +
+               ( keyPair.getPublic() != null ? " ka qeles publik"  : " nuk ka qeles publik");
     }
 }
